ce822fe6060bcae7fcd5c470dd0a17a5bd266b6d
sajwanmayank <sajwanmayank009@gmail.com>
2025-05-31 15:57:18 +0530

    Update format_query.py

--- format_query.py
+++ format_query.py
@@ -16,12 +16,3 @@ def format_query(user_prompt: str, column_dtype_dict: dict,temperature: float = 
         model="deepseek-r1-distill-llama-70b"
     )
     return response["content"]
-
-if __name__ == "__main__":
-    user_query = "what is data analyse "
-    file_name = "dummy_sales_data.csv"  # Example filename
-    import pandas as pd
-    df=pd.read_csv(file_name)
-    dtype_dict = df.dtypes.to_dict()  # Automatically get columns from th  # Example columns
-    formatted_query = format_query(user_query,dtype_dict)
-    print(f"Formatted Query:{formatted_query}")
